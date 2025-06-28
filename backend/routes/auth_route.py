65171919bbc7bd8fd0430f9aa4e54a6f1fc327b8
Nanda Lal Das <tikludas01@gmail.com>
2025-06-28 16:52:21 +0530

    signup/login route fixed

--- backend/routes/auth_route.py
+++ backend/routes/auth_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from models.user_model import UserCreate, UserLogin, UserResponse
-from controllers.auth_controller import signup, signin
+from controllers.auth_controller import signup, signin, get_user_by_id
 
 router = APIRouter()
 
@@ -12,6 +12,6 @@ async def register_user(user: UserCreate):
 async def login_user(user: UserLogin):
     return await signin(user)
 
-@router.get("/user/{user_id}", response_model=UserWithId)
-async def get_user(user_id: str = Path(..., description="MongoDB user _id")):
-    return await get_user_by_id(user_id)
+# @router.get("/user/{user_id}", response_model=UserResponse)
+# async def get_user(user_id: str = Path(..., description="MongoDB user _id")):
+#     return await get_user_by_id(user_id)
